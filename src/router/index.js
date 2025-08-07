6fe81bd6e55bdd8209f1eff6f6138dd2594b4b62
Senkin219 <ringjoys@gmail.com>
2025-08-07 22:08:39 +0800

    add title for character pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CharacterView from '../views/CharacterView.vue'
 import ListView from '../views/ListView.vue'
-import characterList from "../components/CharacterList"
+import characterList from "../components/CharacterList.json"
+import characterInfo from "../components/CharacterInfo.json"
+
+const characterNameMap = (() => {
+  const caseInsensitiveKeyMap = Object.keys(characterList).reduce((acc, key) => {
+    acc[key.toLowerCase()] = key
+    return acc
+  }, {})
+  const nameMap = {}
+  characterInfo.forEach(info => {
+    const baseKey = info.cardFace.replace("UI_Gcg_CardFace_Char_", "")
+    let zhName = info.name
+    let enName = info.englishName
+    if (info.cardFace === "UI_Gcg_CardFace_Char_Avatar_FurinaOusia") {
+        zhName = "芙宁娜(芒)"
+        enName = "Furina (Ousia)"
+    }
+    const lowerBaseKey = baseKey.toLowerCase()
+    if (caseInsensitiveKeyMap[lowerBaseKey]) {
+      const originalKey = caseInsensitiveKeyMap[lowerBaseKey]
+      nameMap[originalKey] = { zh: zhName, en: enName }
+    }
+    const lowerKey02 = (baseKey + '02').toLowerCase()
+    if (caseInsensitiveKeyMap[lowerKey02]) {
+      const originalKey02 = caseInsensitiveKeyMap[lowerKey02]
+      nameMap[originalKey02] = { zh: `${zhName}02`, en: `${enName}02` }
+    }
+  })
+  return nameMap
+})()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,7 +61,15 @@ const router = createRouter({
         else next(import.meta.env.BASE_URL)
       },
       meta: {
-        title: route => `${route.params.name.replace('Avatar_', '').replace('Monster_', '')}`
+        title: route => {
+          const charKey = route.params.name
+          const characterNames = characterNameMap[charKey]
+          const isChinese = navigator.language.startsWith('zh')
+          if (characterNames) {
+            return isChinese ? characterNames.zh : characterNames.en
+          }
+          return charKey.replace('Avatar_', '').replace('Monster_', '')
+        }
       }
     }
   ]
