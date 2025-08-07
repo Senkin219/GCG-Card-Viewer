25cbbcb007e47f592cbf9b6864426d48e7222117
Senkin219 <ringjoys@gmail.com>
2025-08-08 03:04:42 +0800

    add pwa configs

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,68 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     ElementPlus({}),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg,gif}'],
+        globIgnores: ['resources/**'],
+        runtimeCaching: [
+          {
+            urlPattern: /\/resources\/.*\.(atlas|skel|json|png|webp)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'spine-model-cache',
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'GCG Card Viewer',
+        short_name: 'GCG Viewer',
+        description: 'A Genius Invokation TCG dynamic card viewer.',
+        theme_color: '#6c4d36',
+        icons: [
+          {
+            'src': 'pwa-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png',
+            'purpose': 'any'
+          },
+          {
+            'src': 'pwa-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png',
+            'purpose': 'any'
+          },
+          {
+            'src': 'pwa-maskable-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png',
+            'purpose': 'maskable'
+          },
+          {
+            'src': 'pwa-maskable-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png',
+            'purpose': 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
