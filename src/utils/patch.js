9c2fc7dc2c8a7751741729f220aac07dea2bb159
Senkin219 <ringjoys@gmail.com>
2025-12-15 22:41:50 +0800

    remove unused attributes

--- src/utils/patch.js
+++ src/utils/patch.js
@@ -22,12 +22,8 @@ SpinePlayer.prototype.drawFrame = function (requestNextFrame = true) {
   const targetFPS = window.WEconfigs && window.WEconfigs.fps;
   if (targetFPS) {
     const interval = 1.0 / targetFPS;
-    if (!this.last) this.last = performance.now() / 1000;
-    if (!this.fpsThreshold) this.fpsThreshold = 0;
     const now = performance.now() / 1000;
-    if (!this._lastDrawTime) {
-      this._lastDrawTime = now;
-    }
+    if (!this._lastDrawTime) this._lastDrawTime = now;
     const elapsed = now - this._lastDrawTime;
     if (elapsed < interval) {
       requestAnimationFrame(() => this.drawFrame());
